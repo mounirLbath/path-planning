40093f87192a23d5ff7955b6aa9cf6cf97aabb5a
Elyndrium <91188350+octavegg@users.noreply.github.com>
2026-02-05 11:11:15 +0100

    Optimize nearest-neighbor search with the grid as well

--- RRT.py
+++ RRT.py
@@ -29,15 +29,39 @@ def sample_random_point(problem: Problem) -> Point:
     return Point(random.uniform(0, problem.xmax), random.uniform(0, problem.ymax))
 
 
-def nearest_node_index(nodes: list[Node], point: Point) -> int:
-    i_near = 0
+def nodes_around(
+    grid_y_x: list[list[list[int]]],
+    point: Point,
+    delta_r: float,
+    grid_zone: list[tuple[int, int]] = [(i, j) for i in (-1, 0, 1) for j in (-1, 0, 1)],
+) -> list[int]:
+    indices = []
+    for i, j in grid_zone:
+        if 0 <= int(point.y // delta_r) + i < len(grid_y_x) and 0 <= int(point.x // delta_r) + j < len(grid_y_x[0]):
+            indices.extend(grid_y_x[int(point.y // delta_r) + i][int(point.x // delta_r) + j])
+    return indices
+
+
+def nearest_node_index(nodes: list[Node], grid_y_x: list[list[list[int]]], delta_r: float, point: Point) -> int:
+    i_best = 0
     d_best = float("inf")
-    for i, node in enumerate(nodes):
-        d = distance(node.point, point)
-        if d < d_best:
-            d_best = d
-            i_near = i
-    return i_near
+    # Square approximation; at worst we look for nodes sqrt(2) times too far away
+    for grid_distance in range(0, max(len(grid_y_x), len(grid_y_x[0]))):
+        grid_zone = [
+            (i, j)
+            for i in range(-grid_distance, grid_distance + 1)
+            for j in range(-grid_distance, grid_distance + 1)
+            if max(abs(i), abs(j)) == grid_distance
+        ]
+        for node_index in nodes_around(grid_y_x, point, delta_r, grid_zone):
+            d = distance(nodes[node_index].point, point)
+            if d < d_best:
+                d_best = d
+                i_best = node_index
+        # We may find a grid_distance 1 with distance 0, but grid_distance 2 will have dist > delta_r
+        if d_best <= (grid_distance - 1) * delta_r:
+            break
+    return i_best
 
 
 def crop_vr(v_near: Point, v_rand: Point, delta_s: float) -> Point:
@@ -55,15 +79,6 @@ def reconstruct_path(nodes: list[Node], index: int) -> list[Point]:
         index = nodes[index].parent
     path.reverse()
     return path
-
-
-def nodes_around(grid_y_x: list[list[list[int]]], point: Point, delta_r: float) -> list[int]:
-    indices = []
-    for i in (-1, 0, 1):
-        for j in (-1, 0, 1):
-            if 0 <= int(point.y // delta_r) + i < len(grid_y_x) and 0 <= int(point.x // delta_r) + j < len(grid_y_x[0]):
-                indices.extend(grid_y_x[int(point.y // delta_r) + i][int(point.x // delta_r) + j])
-    return indices
 
 
 def rewire_nodes(
@@ -132,7 +147,6 @@ def rrt(
         [[] for i in range(int(problem.xmax / delta_r) + 1)] for j in range(int(problem.ymax / delta_r) + 1)
     ]
     grid_y_x[int(problem.start1.y // delta_r)][int(problem.start1.x // delta_r)].append(0)
-    COSTS["grid_init"] = time.time() - timer
 
     for _ in range(max_iters):
         check_infinite = 0
@@ -142,13 +156,13 @@ def rrt(
             # Only sample points that could improve the path to the goal
             while goal and distance(problem.start1, v_r) + distance(v_r, problem.goal1) >= nodes[goal].cost:
                 v_r = sample_random_point(problem)
-            COSTS["sampling"] = time.time() - timer
+            COSTS["sampling"] = COSTS.get("sampling", 0) + time.time() - timer
 
             # nearest node
             timer = time.time()
-            i_n = nearest_node_index(nodes, v_r)
+            i_n = nearest_node_index(nodes, grid_y_x, delta_r, v_r)
             v_n = nodes[i_n].point
-            COSTS["nearest"] = time.time() - timer
+            COSTS["nearest"] = COSTS.get("nearest", 0) + time.time() - timer
 
             # crop random point within delta_s
             v_new = crop_vr(v_n, v_r, delta_s)
@@ -163,6 +177,7 @@ def rrt(
 
         # choose best parent within delta_r for v_new
         # we check the 9 grid cells around v_new and that makes a sufficient condition (picking a parent further than delta_r with better total distance would still be optimal, and delta_r is here just to improve the efficiency of the algorithm)
+        timer = time.time()
         best_parent = i_n
         best_cost = nodes[i_n].cost + distance(v_n, v_new)
         for index in nodes_around(grid_y_x, v_new, delta_r):
@@ -177,20 +192,27 @@ def rrt(
         nodes[best_parent].children.add(len(nodes) - 1)
         grid_y_x[int(v_new.y // delta_r)][int(v_new.x // delta_r)].append(len(nodes) - 1)
         i_new = len(nodes) - 1
+        COSTS["best_parent"] = COSTS.get("best_parent", 0) + time.time() - timer
 
         # rewiring nodes close enough to v_new similarly
+        timer = time.time()
         rewire_from = i_new
         rewired_nodes, rewired_goal = rewire_nodes(nodes, grid_y_x, problem, delta_r, rewire_from, goal)
         if rewired_goal:
             last_optimized_step = len(nodes) - 1
+        COSTS["rewire"] = COSTS.get("rewire", 0) + time.time() - timer
 
         # Custom addition: rewire recursively if asked
+        if recursive_rewire:
+            timer = time.time()
         while recursive_rewire and len(rewired_nodes) > 0:
             rewire_from = rewired_nodes.pop()
             rewired_nodes, rewired_goal = rewire_nodes(nodes, grid_y_x, problem, delta_r, rewire_from, goal)
             rewired_nodes.extend(rewired_nodes)
             if rewired_goal:
                 last_optimized_step = len(nodes) - 1
+        if recursive_rewire:
+            COSTS["recursive_rewire"] = COSTS.get("recursive_rewire", 0) + time.time() - timer
 
         # goal check
         if not segment_collision(v_new, problem.goal1, problem.obstacles) and (
@@ -266,24 +288,31 @@ if __name__ == "__main__":
     # set random seed
     random.seed(1)
     timer = time.time()
-    prob = load_problem("./scenarios/scenario0.txt")
+    prob = load_problem("./scenarios/scenario4.txt")
     print(f"Environment loaded in {time.time() - timer:.2f} seconds")
     timer = time.time()
     path, steps_taken, last_optimized_step = rrt(
         prob,
         delta_s=20.0,
-        delta_r=30.0,
-        max_iters=1000,
+        delta_r=50.0,
+        max_iters=5000,
         recursive_rewire=True,
         optimize_after_goal=True,
         display_tree_end=False,
     )
-    print(f"RRT completed in {time.time() - timer:.2f} seconds")
+    print(f"RRT completed in {time.time() - timer:.2f} seconds. Decomposition of costs:")
+    for k, v in COSTS.items():
+        print(f"  {k}: {v:.4f} seconds")
+    print("Total accounted time: ", sum(COSTS.values()), " seconds")
     if path is not None:
         display_environment(prob, path)
         print(
-            f"Path found with {len(path)} points and total length {sum(distance(path[i], path[i + 1]) for i in range(len(path) - 1)):.2f}, found in {steps_taken} steps"
-            + ("" if last_optimized_step == steps_taken else f", last optimized step: {last_optimized_step}")
+            f"Path found with {len(path)} points and total length {sum(distance(path[i], path[i + 1]) for i in range(len(path) - 1)):.2f}"
+            + (
+                f", found in {steps_taken} steps"
+                if last_optimized_step == steps_taken
+                else f", last optimized step: {last_optimized_step}"
+            )
         )
     else:
         print("No path found")
