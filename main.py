3a3235cff4f38630e55c82943d70737f6451b17e
Mounir LBATH <100532921+devmlGit@users.noreply.github.com>
2026-02-11 19:16:04 +0100

    Add comments Add some questions in the report

--- main.py
+++ main.py
@@ -8,37 +8,24 @@ if __name__ == "__main__":
     
     def fitness(path: Path) -> float:
         penalty = 10000.
-        #return  (path.nb_pair_collision(problem.obstacles),path.length())
         return path.nb_pair_collision(problem.obstacles) * penalty + path.length()
     
-    # overall_best = 100000000
-    # for T in range(100, 1000, 100):
-    #     for beta in range(5, 100, 2):
-    #         print(f"{T} and {beta} #########################################")
-    #         best_f, _ = particle_swarm_optimization(problem,fitness,max_iter=100,S = 500,random_restart=True, simulated_annealing=True, beta = beta/100,T0=T)
-    #         if best_f < overall_best:
-    #             overall_best = best_f
-
-    # best_f, g = particle_swarm_optimization(problem,fitness, random_restart=True, simulated_annealing=True, T0=10000,beta=0.8,dim_learning=True, nb_update_before_dim=20)
     t0 = time.time()
 
     best_f, g = particle_swarm_optimization(
-    problem,
-    fitness,
-    max_iter=300,
-    S=300,
-    c1=2.0,
-    c2=1.2,
-    w = 0.8,
-    random_restart=True,
-    random_period=15,
-    simulated_annealing=True,
-    dim_learning=True,
-    nb_update_before_dim=15
-)
+        problem,
+        fitness,
+        max_iter=300,
+        S=300,
+        c1=2.0,
+        c2=1.2,
+        w = 0.8,
+        random_restart=True,
+        random_period=15,
+        simulated_annealing=True,
+        dim_learning=True,
+        nb_update_before_dim=15
+    )
 
     print(f"total time:{time.time()-t0}")
-    # for p in g.points:
-    #     print(p)
-    # display_environment(problem, path=g)
 
--- pso.py
+++ pso.py
@@ -7,13 +7,16 @@ from math import exp
 random.seed(42)
 
 def random_path(problem: Problem, nb_points_path:int) -> Path:
+    """random path"""
+    return Path([Point(random.random()*problem.xmax, random.random()*problem.ymax) for i in range(nb_points_path)], problem.start1, problem.goal1)
+
+def semi_random_path(problem: Problem, nb_points_path:int) -> Path:
+    """random path sampled with gaussian around the line y = x"""
     sigma_sq = 400
     return Path([Point(random.gauss(0, sigma_sq)+(i+1)*problem.xmax/(nb_points_path+1), random.gauss(0, sigma_sq)+(i+1)*problem.ymax/(nb_points_path+1)).clamp(problem.xmax, problem.ymax)for i in range(nb_points_path)], problem.start1, problem.goal1)
 
-def prog_path(problem:Problem, nb_points_path:int, exponent: float) -> Path:
-    return Path([Point((i+1)*problem.xmax/(nb_points_path+1), problem.xmax*((i+1)/(nb_points_path+1))**exponent).clamp(problem.xmax, problem.ymax)for i in range(nb_points_path)], problem.start1, problem.goal1)
-
 def symmetric_prog_path(problem: Problem, nb_points_path: int, i: int, S: int) -> Path:
+    """random paths sampled curves that roughly span the whole environment"""
     exponent = (i // 2 + 1)  / ((S + 1) // 2)
     points = []
     for k in range(nb_points_path):
@@ -32,7 +35,9 @@ def symmetric_prog_path(problem: Problem, nb_points_path: int, i: int, S: int) -
 
 
 def particle_swarm_optimization(problem : Problem, 
-                                fitness, max_iter: int = 100, 
+                                fitness, 
+                                max_iter: int = 100, 
+                                nb_points_path = 10,
                                 S: int = 500, # nb of particles
                                 c1: float = 1.5, 
                                 c2: float = 1.5,
@@ -43,46 +48,59 @@ def particle_swarm_optimization(problem : Problem,
                                 beta: float = 0.95,
                                 T0: float = 10000,
                                 dim_learning: bool = False,
-                                nb_update_before_dim: int = 5 
+                                nb_update_before_dim: int = 5, 
+                                DEBUG: bool = True
                                 ) -> Path:
     """
     Returns the path found by the particle swarm optimization algorithm
     max_iter: maximum number of iterations before stopping the algorithm
+    nb_points_path: number of points on the discretized path
+    fitness: the fitness function (takes a Path and returns its fitness value)
     S: number of particles to simulate
     c1 and c2: acceleration coefficients
     w: inertia weight for velocity
+    random_restart: set to True to include random restart optimization
+    random_period: set only if random_restart=True
+    simulated_annealing: set to True to include simulated annealing
+    beta and T0: parameters for simulated annealing
+    dim_learning: set to True to include dimension learning optimization
+    nb_update_before_dim: time to wait without any updates of P[i] before enforcing dimension learning 
+    DEBUG: show logs if True
     """
 
-    nb_points_path = 10
 
-    #X = [Path([Point(random.random()*problem.xmax, random.random()*problem.ymax) for i in range(nb_points_path)], problem.start1, problem.goal1) for k in range(S)]
+    # initialize paths
     X = [symmetric_prog_path(problem, nb_points_path, k, S) for k in range(S)]
+
     display_environment(problem, path=X[150])
-    
+
     V = [Path([Point(0,0) for i in range(nb_points_path)], problem.start1, problem.goal1) for k in range(S)]
     P = [x for x in X]
     g_index = min(range(S), key=lambda i: fitness(P[i])) # index of the particle with global best position
     best_f = fitness(P[g_index])
 
+    # initialize temperature for simulated annealing
     T = T0
+
+    # stores the last time P[i] was updated for each i
     last_updated = [0] * S
 
     for k in range(max_iter):
         for i in range(S): 
             r1 = random.random()
             r2 = random.random()
+            # update particle
             if random_restart and random_period > 0 and k > 0 and k%random_period == 0:
-                X[i] = symmetric_prog_path(problem, nb_points_path, i, S)#random_path(problem, nb_points_path)
+                # reset to random path if random restart is activated
+                X[i] = symmetric_prog_path(problem, nb_points_path, i, S)
                 V[i] =Path([Point(0,0) for i in range(nb_points_path)], problem.start1, problem.goal1) 
             else:
                 V[i] = (V[i]*w + (P[i]-X[i])*c1*r1 + (P[g_index]-X[i])*c2*r2).clamp_norm(60)
                 X[i] = (X[i] + V[i]).clamp(problem.xmax, problem.ymax)
 
-
             curr_fitness = fitness(X[i])
-            
-            
 
+            # dimension learning
             if dim_learning and k - last_updated[i] >= nb_update_before_dim:
                 new_path = Path([x for x in X[i].points], X[i].start, X[i].end)
                 for j in range(nb_points_path):
@@ -95,24 +113,26 @@ def particle_swarm_optimization(problem : Problem,
                         new_path.points[j] = old_point
                 X[i] = new_path
         
+            # update particle's best solution
             if curr_fitness < fitness(P[i]):
                 P[i] = X[i]
                 last_updated[i] = k
 
+        # update temperature
         if simulated_annealing:
             T *= beta
 
+        # update global best solution
         for i in range(S):
             curr_f = fitness(P[i])
             if simulated_annealing and T >= 0.01:
-                delta = curr_f - best_f#(curr_f[0]*1000 + curr_f[1] - best_f[0]*1000 - best_f[1])
+                delta = curr_f - best_f
                 if curr_f <= best_f:
                     prob = 1
                 elif delta < T*10.:
                     prob = exp(-delta/T)
-                else:
+                else: # exp(-delta/T) ~ 0
                     prob = 0
-
 
                 u = random.uniform(0,1)
                 if u <= prob:
@@ -123,8 +143,8 @@ def particle_swarm_optimization(problem : Problem,
                     g_index = i
                     best_f = curr_f
 
-
-        print(best_f)
+        if DEBUG:
+            print(best_f)
 
     display_environment(problem, paths=P, path=P[g_index])
     return best_f, P[g_index]
